d341ab480b9890aa625a2a45d4b41be3d926ac2f
Colin Bielen <colin.bieln@gmail.com>
2011-04-11 21:55:58 -0700

    Re-committed cuz i forgot git != svn

--- ToDoList/gen/com/bielen/colin/todolist/R.java
+++ ToDoList/gen/com/bielen/colin/todolist/R.java
@@ -10,19 +10,44 @@ package com.bielen.colin.todolist;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int Black=0x7f040001;
+        public static final int Blue_opaque=0x7f040005;
+        public static final int Gray=0x7f040007;
+        public static final int Gray_DARK=0x7f040008;
+        public static final int Green_translucent=0x7f040006;
+        public static final int Red_bright=0x7f040003;
+        public static final int Red_opaque=0x7f040002;
+        public static final int Red_translucent=0x7f040004;
+        public static final int White=0x7f040000;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int EditText01=0x7f050001;
-        public static final int LinearLayout01=0x7f050000;
-        public static final int ListView01=0x7f050002;
+        public static final int EditText01=0x7f070002;
+        public static final int LinearLayout01=0x7f070000;
+        public static final int LinearLayout02=0x7f070003;
+        public static final int LinearLayout03=0x7f070007;
+        public static final int ListView01=0x7f070005;
+        public static final int RelativeLayout01=0x7f070001;
+        public static final int RelativeLayout02=0x7f070006;
+        public static final int TextView01=0x7f070004;
+        public static final int TextView02=0x7f070008;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_instructions=0x7f050003;
+        public static final int app_name=0x7f050005;
+        public static final int app_title=0x7f050004;
+        public static final int button_add_label=0x7f050001;
+        public static final int edit_text_instructions=0x7f050000;
+        public static final int label_current_list=0x7f050002;
+    }
+    public static final class style {
+        public static final int EditTextStyle=0x7f060001;
+        public static final int ToDoTheme=0x7f060000;
     }
 }
--- ToDoList/src/com/bielen/colin/todolist/ToDoList.java
+++ ToDoList/src/com/bielen/colin/todolist/ToDoList.java
@@ -5,52 +5,101 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+
 /**
- * Way simple ToDoList to see what android is all about. 
- * ...and to try eclipse for a java project instead of just git/svn browsing and C/C++
+ * Way simple ToDoList to see what android is all about. ...and to try eclipse
+ * for a java project instead of just git/svn browsing and C/C++
  * 
  * @author cbielen
- *
+ * 
  */
 public class ToDoList extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //UI Setup:
-        
-        setContentView(R.layout.main);
-        ListView myListView = (ListView)findViewById(R.id.ListView01);
-        final EditText myEditText = (EditText)findViewById(R.id.EditText01);
-        final List<String> toDoItems = new ArrayList<String>();
-        final ArrayAdapter<String> aa;
-        aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,toDoItems);
+	private static final String TAG = "ToDoList";
 
-        myListView.setAdapter(aa);
-        //Add the Key Listener and Stuff.
-        
-        myEditText.setOnKeyListener(new OnKeyListener() {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.d(TAG, "onCreate");
+		// UI Setup:
+
+		setContentView(R.layout.main);
+		ListView myListView = (ListView) findViewById(R.id.ListView01);
+		final EditText myEditText = (EditText) findViewById(R.id.EditText01);
+		final List<String> toDoItems = new ArrayList<String>();
+		final ArrayAdapter<String> aa;
+		aa = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, toDoItems);
+
+		myListView.setAdapter(aa);
+		// Add the Key Listener and Stuff.
+
+		myEditText.setOnKeyListener(new OnKeyListener() {
 
 			public boolean onKey(View v, int keyCode, KeyEvent event) {
-				
-				if(KeyEvent.ACTION_DOWN == event.getAction()) {
-					if(keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
-						
-						toDoItems.add(myEditText.getText().toString());
-						aa.notifyDataSetChanged();
+
+				if (KeyEvent.ACTION_DOWN == event.getAction()) {
+					if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
+						Log.d(TAG, "Adding Thingee to List View...");
+						String enteredText = myEditText.getText().toString();
+						if (enteredText.length() > 0) {
+							toDoItems.add(enteredText);
+							aa.notifyDataSetChanged();
+							Log.d(TAG, "Empty String passed in. Skipping");
+						}
+
 						myEditText.setText("");
 						return true;
-					} //end DPADCENTER check.					
-				} //end ACTION_DOWN check. 			
+					} // end DPADCENTER check.
+				} // end ACTION_DOWN check.
 				return false;
-			}//end onKey()
-        	
-        });//end setOnKeyListener()
-    } //end onCreate()
-} //end ToDoList class
+			}// end onKey()
+
+		});// end setOnKeyListener()
+	} // end onCreate()
+
+	@Override
+	protected void onDestroy() {
+
+		super.onDestroy();
+		Log.d(TAG, "onDestroy()");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.d(TAG, "onPause()");
+	}
+
+	@Override
+	protected void onRestart() {
+		super.onRestart();
+		Log.d(TAG, "onRestart()");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d(TAG, "onResume()");
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.d(TAG, "onStart()");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.d(TAG, "onStop()");
+	}
+
+} // end ToDoList class
